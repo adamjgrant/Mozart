ff4ca3825e725e00d466c5ffb500d5d1fd635007
Adam Grant <ajkochanowicz@gmail.com>
2016-04-19 16:34:13 -0700

    Allows for calling without second fn parameter to just get the component selector.

--- scoped_component.js
+++ scoped_component.js
@@ -1,11 +1,11 @@
 $.component = function(selectors, fn) {
   var selector = [""].concat((selectors).split(" ")).reduce(function(a, b) {
-        return a + '[data-component~="' + b + '"]'
+        return a + '[data-component~="' + b + '"]';
       }),
       $selector = $(selector),
       getInScope = function(scoped_selector) {
         return $selector.find(scoped_selector);
       };
 
-  return fn.call(this, getInScope);
+  return (fn === undefined ? $selector : fn.call(this, getInScope));
 }
