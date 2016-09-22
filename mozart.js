dad2a6680408f6ba069ae2508c387f10bc2891e1
Adam Kochanowicz <ajkochanowicz@gmail.com>
2016-09-22 14:49:36 -0700

    Adding basic test file

---
+++ mozart.js
@@ -0,0 +1,8 @@
+// Constructor
+var Mozart = new Function,
+Mozart.prototype.router = function() {};
+Mozart.prototype.router = function() {};
+
+// Instance, assign to both "mozart" and "_$"
+var mozart = new Mozart,
+    _$ = mozart;
