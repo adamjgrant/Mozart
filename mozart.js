379f976d855a1681d26b4a766e2a89bca17e91b3
Adam Kochanowicz <ajkochanowicz@gmail.com>
2016-09-23 11:25:55 -0700

    Actually runs tests

--- mozart.js
+++ mozart.js
@@ -1,104 +1,102 @@
 // Constructor
-Mozart = (function($) {
-  return {
-    init: function(_components) {
-      var components = [];
+var Mozart = {
+  init: function(_components) {
+    var components = [];
 
-      var format_component_as_class_and_instance = function(index, _component) {
-        component_class = component_instance = _component;
+    var format_component_as_class_and_instance = function(index, _component) {
+      component_class = component_instance = _component;
 
-        if (typeof(_component) != "string") {
-          for (key in _component) {
-            component_class = key;
-            component_instance = _component[key];
-          }
-        }
-        else if (typeof(_component) != "object" && typeof(_component) != "string") {
-          console.error(_component, "not a valid init object. Must be a string or {class:instance}")
-        }
-
-        components.push({ class: component_class, instance: component_instance });
-      }
-
-      $.each(_components, format_component_as_class_and_instance);
-      $.each(components, function(index, component) {
-        var html_name = this.parameterize(component["class"], true),
-            js_name = this.parameterize(component["class"], false),
-            component_selectors = html_name;
-
-        // In case the user didn't define one or more of these
-        $.each(["events", "api", "config"], function(index, prop) {
-          window[js_name][prop] = window[js_name][prop] || {}
-        });
-
-        // Push _$ through to the component's events function
-        // TODO: Not sure this is the best way to go. Race condition with user's event
-        // function being defined. Maybe we need to rethink syntax.
-        var router = this.create_router_for(js_name, window[js_name].config["router"]);
-        this.set_scope(component_selectors, window[js_name].events, window[js_name], router);
-        this.set_scope(component_selectors, window[js_name].api, window[js_name], router);
-      }.bind(this));
-    },
-
-    create_router_for: function(name, config) {
-      config = $.extend(true,
-        {
-          base_url: "/",
-          base_name: name
-        },
-        config
-      );
-      root_url = config.base_url + name;
-
-      return {
-        index: {
-          url: root_url + ".json",
-          method: "GET"
-        },
-        show: {
-          url: root_url + "/:id.json",
-          method: "GET"
-        },
-        destroy: {
-          url: root_url + "/:id.json",
-          method: "DELETE"
-        },
-        update: {
-          url: root_url + "/:id",
-          method: "PUT"
-        },
-        new: {
-          url: root_url + "/new.json",
-          method: "GET"
-        },
-        create: {
-          url: root_url + "/:id",
-          method: "POST"
+      if (typeof(_component) != "string") {
+        for (key in _component) {
+          component_class = key;
+          component_instance = _component[key];
         }
       }
-    },
+      else if (typeof(_component) != "object" && typeof(_component) != "string") {
+        console.error(_component, "not a valid init object. Must be a string or {class:instance}")
+      }
 
-    set_scope: function(selectors, fn, component_js, router) {
-      var selector = [""].concat((selectors).split(" ")).reduce(function(a, b) {
-            return a + '[data-component~="' + b + '"]';
-          }),
-
-      // TODO: _$ also needs .api, .router. and .config
-      _$ = function(scoped_selector) {
-        _jQuery = $(selector + " " + scoped_selector);
-        _jQuery.api = component_js.api // TODO: Not sure if this is enough.
-        _jQuery.router = router
-        return _jQuery;
-      };
-
-      return (fn === undefined ? $selector : fn.call(this, _$));
-    },
-
-    parameterize: function(str, dashes) {
-      var separator = dashes ? "-" : "_"
-      return str.toLowerCase().replace(/[^a-z0-9]+/g,separator).replace(/(^-|-$)/g,'');
+      components.push({ class: component_class, instance: component_instance });
     }
-  }
-})($);
 
-module.exports = Mozart;
+    $.each(_components, format_component_as_class_and_instance);
+    $.each(components, function(index, component) {
+      var html_name = this.parameterize(component["class"], true),
+          js_name = this.parameterize(component["class"], false),
+          component_selectors = html_name;
+
+      if (!window[js_name]) { console.error("Could not find the base component variable " + js_name) }
+
+      // In case the user didn't define one or more of these
+      $.each(["events", "api", "config"], function(index, prop) {
+        window[js_name][prop] = window[js_name][prop] || {}
+      });
+
+      // Push _$ through to the component's events function
+      // TODO: Not sure this is the best way to go. Race condition with user's event
+      // function being defined. Maybe we need to rethink syntax.
+      var router = this.create_router_for(js_name, window[js_name].config["router"]);
+      this.set_scope(component_selectors, window[js_name].events, window[js_name], router);
+      this.set_scope(component_selectors, window[js_name].api, window[js_name], router);
+    }.bind(this));
+  },
+
+  create_router_for: function(name, config) {
+    config = $.extend(true,
+      {
+        base_url: "/",
+        base_name: name
+      },
+      config
+    );
+    root_url = config.base_url + name;
+
+    return {
+      index: {
+        url: root_url + ".json",
+        method: "GET"
+      },
+      show: {
+        url: root_url + "/:id.json",
+        method: "GET"
+      },
+      destroy: {
+        url: root_url + "/:id.json",
+        method: "DELETE"
+      },
+      update: {
+        url: root_url + "/:id",
+        method: "PUT"
+      },
+      new: {
+        url: root_url + "/new.json",
+        method: "GET"
+      },
+      create: {
+        url: root_url + "/:id",
+        method: "POST"
+      }
+    }
+  },
+
+  set_scope: function(selectors, fn, component_js, router) {
+    var selector = [""].concat((selectors).split(" ")).reduce(function(a, b) {
+          return a + '[data-component~="' + b + '"]';
+        }),
+
+    // TODO: _$ also needs .api, .router. and .config
+    _$ = function(scoped_selector) {
+      _jQuery = $(selector + " " + scoped_selector);
+      _jQuery.api = component_js.api // TODO: Not sure if this is enough.
+      _jQuery.router = router
+      return _jQuery;
+    };
+
+    return (fn === undefined ? $selector : fn.call(this, _$));
+  },
+
+  parameterize: function(str, dashes) {
+    var separator = dashes ? "-" : "_"
+    return str.toLowerCase().replace(/[^a-z0-9]+/g,separator).replace(/(^-|-$)/g,'');
+  }
+}
--- tests/test.js
+++ tests/test.js
@@ -3,39 +3,46 @@
 //     mocha  = require("mocha"),
 //     $      = require("jquery");
 //
-// expect(Mozart.init).to.be.a('function');
 //
-// // 1st Class INSTANCE VARIABLE
 //
-// var test_component = {},
-//     _$_outside_api,
-//     _$_outside_event,
-//     _$_outside_router;
 //
-// test_component.events = function(_$) {
-//   _$("p").click(function() { return "foo"; });
-//   _$_outside_event = _$;
-//   _$_outside_router = _$.router;
-// };
 //
-// test_component.api = function(_$) {
-//   _$_outside_api = _$;
 //
-//   return {
-//     create: function(blah) {}
-//   }
-// }
-//
-// Mozart.init([
-//   "test-component"
-// ]);
-//
-// expect(_$_outside_event).to.be.a('function');
-// expect(_$_outside_api).to.be.a('function');
-// expect(_$_outside_router).to.be.a('object');
 
-describe("foo", function() {
-  it("does stuff", function() {
-    expect({}).to.be.a("object");
+describe("Mozart class", function() {
+  it("is defined", function() {
+    expect(Mozart).to.be.a('object');
+    expect(Mozart.init).to.be.a('function');
+  });
+});
+
+describe("Mozart 1st Class Instance Variable", function() {
+  var _$_outside_api,
+      _$_outside_event,
+      _$_outside_router;
+      
+  window.test_component = {};
+
+  test_component.events = function(_$) {
+    _$("p").click(function() { return "foo"; });
+    _$_outside_event = _$;
+    _$_outside_router = _$.router;
+  };
+
+  test_component.api = function(_$) {
+    _$_outside_api = _$;
+  }
+
+  Mozart.init([
+    "test-component"
+  ]);
+
+  it("defines a scoped jQuery (_$)", function() {
+    expect(_$_outside_event).to.be.a('function');
+    expect(_$_outside_api).to.be.a('function');
+  });
+
+  it("defines a router if one is not present", function() {
+    expect(_$_outside_router).to.be.a('object');
   });
 });
