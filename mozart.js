1ba68cc7bcf0ec47e4f07ce4746c897865be7c5a
Adam Grant <adam_grant@apple.com>
2016-09-29 12:28:42 -0700

    Adding a todo

--- mozart.js
+++ mozart.js
@@ -191,6 +191,9 @@ Mozart.Component.prototype.set_scope = function(fn_name_or_function) {
   _$.router   = m$[this.js_name].router;
   _$.selector = selector;
   _$.store    = m$['store'] ? m$.store[this.js_name] : {};
+  // TODO: Lots of confusion about what _$ means. Is it a shortcut to m$.mycomponent? Should it be?
+  // Maybe we could decorate that with the above and then set _$ equal to it.
+  // Although...one problem would be that config stuff wouldn't be picked up at init time.
   var fn = typeof(fn_name_or_function) == "function" ? fn_name_or_function : m$[this.js_name][fn_name_or_function];
   return (fn === undefined ? _$ : $(document).ready(function() {
     fn.call($(selector), _$) }.bind($(selector))
