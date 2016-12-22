816ae974d0626b945d1fc073658d6b5a1738bc58
Adam Kochanowicz <ajkochanowicz@gmail.com>
2016-12-22 00:03:38 -0600

    Writing some tests

--- tests/test-jquery.js
+++ tests/test-jquery.js
@@ -11,7 +11,7 @@ describe("Mozart class", function() {
 describe("_$ scoped jquery", function() {
   var $h1 = undefined;
 
-  it("you know.", function() {
+  it("return a jQuery object.", function() {
     m$.test = new Mozart;
 
     m$.test.api({
@@ -23,5 +23,22 @@ describe("_$ scoped jquery", function() {
     Mozart.init();
     m$.test.api.thing();
     expect($h1.__proto__.jquery).to.be.a("String");
+  });
+
+  var self = undefined;
+
+  it("api lookups work with _$.api", function() {
+    m$.test = new Mozart;
+
+    m$.test.api({
+      thing: function(_$, options) {
+        self  = _$.api.other_thing();
+      },
+      other_thing: function(_$, options) { return true; }
+    });
+
+    Mozart.init();
+    m$.test.api.thing();
+    expect(self).to.eq(true);
   });
 });
