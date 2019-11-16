083a8d46f9c082d701de22bd87a396076b3178ea
Adam Grant <adamgrant@Grant-iMac.local>
2019-11-16 12:23:26 -0800

    Fixing tests

--- docs/events/tests.js
+++ docs/events/tests.js
@@ -3,14 +3,19 @@ doc({
   tests: [
     test("Events are scoped", function() {
       var button = [];
+      var was_i_called = "not called";
       class Component extends Mozart {};
       var generator = new Component;
-      generator.events(_$ => { button = _$("button"); });
+      generator.events(_$ => { 
+        was_i_called = "called";
+        button = _$("button"); 
+      });
 
       Mozart.init();
       
       return [
         assert("Button can be selected with scoped selector", button.length, 1)
+        , assert("Event function is called on Mozart init", was_i_called, "called")
       ]
     })
   ],
