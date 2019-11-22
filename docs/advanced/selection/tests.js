cbb0445d073025d765a9756cd04c4731167b9b02
Adam Grant <hello@adamgrant.me>
2019-11-22 11:51:05 -0800

    Creating failing tests

--- docs/advanced/selection/tests.js
+++ docs/advanced/selection/tests.js
@@ -7,11 +7,45 @@ doc({
       var m = Component.index;
       new Component(["single", "multiple", "multiple_two"]);
 
+      var single_el,
+          multiple_el,
+          multiple_two_el;
+
+      m.single.act({
+        get_element(_$, args) { single_el = _$.me(); }
+      });
+
+      m.multiple.act({
+        get_element(_$, args) { multiple_el = _$.me(); }
+      });
+
+      m.multiple.act({
+        get_element(_$, args) { multiple_two_el = _$.me(args.child); }
+      });
+
+      Mozart.init();
+
+      m.single.act.get_element();
+      m.multiple.act.get_element();
+
+      var child = document.querySelectorAll("[data-component='multiple_two']")[1].querySelector("span");
+      m.multiple_two.act.get_element({ child: child });
+
       return [
-        assert("_$.me() returns one component when only one exists", "", "")
-        , assert("_$.me() returns all components when only multiple exist", "", "")
-        , assert("_$.me(element) returns the right component parent", "", "")
+        assert("_$.me() returns one component when only one exists", typeof(single_el), "node") 
+        , assert("_$.me() returns all components when only multiple exist", multiple_el.length, 3)
+        , assert("_$.me(element) returns the right component parent", false, multiple_two_el.id, 2)
       ]
     })
   ]
-});
+}, `
+  <div data-component="single"></div>
+
+  <div data-component="multiple"></div>
+  <div data-component="multiple"></div>
+  <div data-component="multiple"></div>
+
+  <div data-component="multiple_two" id="1"></div>
+  <div data-component="multiple_two" id="2"><span></span></div>
+  <div data-component="multiple_two" id="3"></div>
+`);
