f4525e45fb38f90a24e4c3c4622a726cc614b390
Adam Grant <hello@adamgrant.me>
2019-11-21 16:33:36 -0800

    Added failing test for bug

--- docs/basics/actions/tests.js
+++ docs/basics/actions/tests.js
@@ -6,18 +6,29 @@ doc({
       var m = Component.index;
       new Component("actionguy"); 
       var iexist = false;
+      var neighbor_response = "";
 
       m.actionguy.acts({
         iexist(_$, args) {
           iexist = true;
-        }
+        },
+
+        value_from_neighbor(_$, args) {
+          neighbor_response = _$.act.neighbor();
+        },
+
+        neighbor(_$, args) {
+          return { "foo": "bar" }
+        },
       });
 
       Mozart.init();
       m.actionguy.act.iexist();
+      m.actionguy.act.value_from_neighbor();
       
       return [
         assert("Action can be called from the outside", iexist, true)
+        , assert("A value returned from a neighbor function comes back through the caller", neighbor_response, { "foo": "bar" })
       ]
     })
   ]
