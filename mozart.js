c55ba939352691c1ba0b0d6d221282ad2a6bec41
Adam Grant <adam_grant@apple.com>
2022-02-15 17:29:12 -0800

    Good stopping point. Everything written should work.

--- mozart.js
+++ mozart.js
@@ -1,14 +1,13 @@
 class Component {
-  constructor() {
+  constructor(name) {
     this.actions = {};
-    this.name = "f";
+    this.name = name;
   }
   
   q(el) {
-    // return `The element passed in was ${el}`;
-    // TODO: Return querySelector scoped to component
-    // const parent_component = document.querySelector(this.name);
-    return `${this.name} ${el}`;
+    const scoped_selector = `[data-component~="${this.name}"] ${el}`;
+    const elements = document.querySelectorAll(scoped_selector);
+    return elements.length > 1 ? elements : elements[0];
   }
 
   get a() {
@@ -17,12 +16,11 @@ class Component {
       get: function(obj, prop) {
         const thing = _.actions[prop];
         const valid = typeof(thing) === "function";
-        return valid ? thing(_.q) : `${_.private.enum.errors.INVALID_ACTION}: "${prop}"`;
+        return valid ? thing.bind(_) : `${_.private.enum.errors.INVALID_ACTION}: "${prop}"`;
       },
       
       set: function(obj, prop, value) {
-        console.log(`${prop} ${value}`);
-        _.private.bootstrap_action_function(prop, value);
+        _.private.bootstrap_action_function.call(_, prop, value);
       }
     };
     
@@ -31,11 +29,15 @@ class Component {
     return p;
   }
   
+  // TODO: Events
+  
   get private() {
     let _ = this;
     return {
-      bootstrap_action_function: (prop, value) => {
-        _.actions[prop] = value;
+      bootstrap_action_function: (prop, fn) => {
+        _.actions[prop] = () => {
+          return fn.call(_, _.q.bind(_));
+        }
       },
       
       enum: {
@@ -47,12 +49,12 @@ class Component {
   }
 }
 
-let foo = new Component();
+let foo = new Component("foo");
 
 foo.a.something = (q) => {
-  return q("foo");
+  return q("bar");
 }
 
-foo.a.something;
+foo.a.something();
 
 foo.a.doesntexist;
