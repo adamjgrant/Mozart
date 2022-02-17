9f650e47f48233b94d2fb08232abfad89c245b73
Adam Grant <adam_grant@apple.com>
2022-02-16 17:15:40 -0800

    Adding some notes

--- docs/about/example/index.js
+++ docs/about/example/index.js
@@ -1,36 +1,38 @@
-let index = new Component("index"),
-    form = new Component("form"),
-    contact = new Component("contact");
+export default UI = new Mozart({
+    form: {},
+    table: {
+        index: {
+            row: {}
+        },
+        contact: {}
+    }
+});
 
-index.components = {
-    row: new Component("index_row")
-}
+// TODO: Potentially confusing because the keys don't have to be unique
+//       but the generated component names in the markup will need to be.
+//
+//       or you make the scoped selector very strict about targeting in
+//       the hierarchy. Side effect is the CSS selectors get very long.
 
-index.act((find) => {
+import UI from "./ui";
+
+// TODO: Oh wait, no you can't do this because you have to call .components
+//       each time you dip down a level. Hmm...
+UI.table.index.act((find) => {
     return {
         create_item(information) {
-            const row = this.components.row.node(information)
+            const row = this.components.row.node(information);
             find.me.appendChild(row);
             this.act.read_item(information.id);
         },
 
         read_item(e) {
-            const id = this.act.private.find_item_id(e);
-            contact.act.show(id);
-        },
-
-        get private() {
-            return {
-                find_item_id(e) {
-                    const id = e.target.dataset.id;
-                    e.stopPropagation();
-                    return id;
-                }
-            }
-        },
+            const id = this.components.row.act.find_item_id(e);
+            UI.table.components.contact.act.show(id);
+        }
     }
 });
 
-index.events((find) => {
+UI.table.index.events((find) => {
     find("a").addEventListener("click", this.act.select_item);
 });
