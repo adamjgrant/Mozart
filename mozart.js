7e593078bc34f618e1e7a004bd82b6c8b348e23f
Adam Grant <hello@adamgrant.me>
2022-08-09 15:06:59 -0700

    Removing scoped_selector

--- mozart.js
+++ mozart.js
@@ -9,15 +9,14 @@ class Component {
                     ThisProxyComponent[key] = val;
                 }
             },
-            scoped_selector(el) {
+            q(el) {
                 let scoped_query = `[data-component~="${this.name}"]`;
                 if (el) scoped_query += ` ${el}`;
                 const elements = document.querySelectorAll(scoped_query);
                 return elements.length > 1 ? Array.from(elements) : elements[0];
             },
 
-            q(...args)     { return this.scoped_selector.apply(this, args); },
-            get me()       { return this.scoped_selector(); },
+            get me()       { return this.q(); },
             register(name) { this.name = name; },
         }
         let ThisProxyComponent = new Proxy(native_methods,
@@ -30,9 +29,7 @@ class Component {
                     let _native_methods = {}
                     for (let key in native_methods) {
                         let val = native_methods[key];
-                        _native_methods[key] = {
-                            value: val, writable: true
-                        }
+                        _native_methods[key] = { value: val, writable: true }
                     }
                     let _value = Object.defineProperties(value, _native_methods);
                     Object.defineProperty(obj, prop, { value: _value.bind(ThisProxyComponent), writable: true });
