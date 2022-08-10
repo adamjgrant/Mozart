e18887a143555ac57df6ade1808794788682f997
Adam Grant <hello@adamgrant.me>
2022-08-09 17:46:29 -0700

    Updating

---
+++ docs/advanced/storing-data/index.js
@@ -0,0 +1,17 @@
+import Component from "mozart.js";
+let widget = new Component("widget");
+
+widget.store.count = 0;
+
+widget.attach({
+  update_count() { 
+    this.store.count++; 
+  },
+
+  show_count() { 
+    this.q(".display").innerHTML = this.store.count;
+  }
+});
+
+const button = widget.q(".add-to-count");
+button.addEventListener("click", widget.update_count);
