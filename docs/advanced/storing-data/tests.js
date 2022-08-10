cd282926cf3169aaa93053aa82b2596745bff7be
Adam Grant <hello@adamgrant.me>
2022-08-09 22:08:11 -0700

    More failing tests

--- docs/advanced/storing-data/tests.js
+++ docs/advanced/storing-data/tests.js
@@ -6,17 +6,31 @@ doc({
             let dropdown_button = new Component("dropdown_button");
 
             dropdown_button.store.marco = "polo";
+            dropdown_button.store.marco_from_setter = "wowo";
 
             dropdown_button.assign({
                 get marco() {
                     return this.store.marco;
+                },
+
+                set set_marco(value) {
+                    this.store.marco_from_setter = value;
+                },
+
+                marco_as_function() {
+                    return this.store.marco;
                 }
             })
+
+            dropdown_button.set_marco = "polo";
 
             return [ 
                 assert("dropdown_button has store", !!dropdown_button.store, true),
                 assert("dropdown_button store readout", dropdown_button.store.marco, "polo"),
-                assert("dropdown_button store accessible from function", dropdown_button.marco, "polo"),
+                assert("dropdown_button store accessible from regular function", dropdown_button.marco_as_function, "polo"),
+                assert("dropdown_button store accessible from getter function", dropdown_button.marco, "polo"),
+                assert("dropdown_button store accessible from setter function", dropdown_button.store.marco_from_setter, "polo"),
+                // TODO do a setter too.
             ]
         },`
         `)
