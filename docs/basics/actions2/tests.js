7d28d3d4f3d881c73595d9945e98d1a54e79fd1a
Adam Grant <hello@adamgrant.me>
2022-08-09 21:47:40 -0700

    Adding more tests

--- docs/basics/actions2/tests.js
+++ docs/basics/actions2/tests.js
@@ -1,47 +1,20 @@
-// doc({
-//     attach_id: "basics/actions",
-//     tests: [
-//         test("Actions", () => {
-//             let navbar = new Component("navbar");
+import Component from "../../../mozart.js";
+let dropdown_button = new Component("dropdown_button");
 
-//             navbar.act.ping = (q) => {
-//                 return ["pong", q("li").length];
-//             }
+dropdown_button.assign({
+  open() { return "open" },
+  close() { return "close" }
+})
 
-//             let navbar2 = new Component("navbar2");
-//             navbar2.act = {
-//                 ping: (q) => { return "pong" },
-//                 pong: (q) => { return "ping" }
-//             }
+doc({
+    attach_id: "basics/actions2",
+    tests: [
+        test("Actions", () => {
 
-//             let navbar3 = new Component("navbar3");
-//             navbar3.act.first_ping = (q) => "first_pong";
-//             navbar3.act = {
-//                 ping: (q) => { return "pong" },
-//                 pong: (q) => { return "ping" }
-//             }
-
-//             return [
-//                 assert("action function responds", navbar.act.ping()[0], "pong"),
-//                 assert("action function employs the scoped selector", navbar.act.ping()[1], 2),
-//                 assert("actions can be assigned with an entire object", [navbar2.act.ping(), navbar2.act.pong()], ["pong", "ping"]),
-//                 assert("single and entire object declaration work gracefully", [
-//                     navbar3.act.first_ping(),
-//                     navbar3.act.ping(),
-//                     navbar3.act.pong()
-//                 ], [
-//                     "first_pong",
-//                     "pong",
-//                     "ping"
-//                 ])
-//             ]
-//         }, `
-//       <nav data-component="navbar">
-//         <ul>
-//           <li><a href="#">Home</a></li>
-//           <li><a href="#">Blog</a></li>
-//         </ul>
-//       </nav>
-//     `)
-//     ]
-// });
+            return [
+               assert("Methods can be added by assignment", [dropdown_button.open(), dropdown_button.close()], ["open", "close"])
+            ]
+        }, `
+    `)
+    ]
+});
