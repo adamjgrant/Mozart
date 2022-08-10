65a2753a57c8b5244bf4619ba12965048d53694b
Adam Grant <hello@adamgrant.me>
2022-08-09 21:56:29 -0700

    More tests

--- docs/basics/calling/tests.js
+++ docs/basics/calling/tests.js
@@ -1,47 +1,24 @@
-// doc({
-//     attach_id: "basics/actions",
-//     tests: [
-//         test("Actions", () => {
-//             let navbar = new Component("navbar");
+import Component from "../../../mozart.js";
+let timer = new Component("timer");
+let alarm = new Component("alarm");
 
-//             navbar.act.ping = (q) => {
-//                 return ["pong", q("li").length];
-//             }
+timer.talk_to_alarm = function() {
+    return alarm.get_status();
+}
 
-//             let navbar2 = new Component("navbar2");
-//             navbar2.act = {
-//                 ping: (q) => { return "pong" },
-//                 pong: (q) => { return "ping" }
-//             }
+alarm.get_status = function() {
+    return "all good"
+}
 
-//             let navbar3 = new Component("navbar3");
-//             navbar3.act.first_ping = (q) => "first_pong";
-//             navbar3.act = {
-//                 ping: (q) => { return "pong" },
-//                 pong: (q) => { return "ping" }
-//             }
+doc({
+    attach_id: "basics/calling",
+    tests: [
+        test("Cross component communication", () => {
 
-//             return [
-//                 assert("action function responds", navbar.act.ping()[0], "pong"),
-//                 assert("action function employs the scoped selector", navbar.act.ping()[1], 2),
-//                 assert("actions can be assigned with an entire object", [navbar2.act.ping(), navbar2.act.pong()], ["pong", "ping"]),
-//                 assert("single and entire object declaration work gracefully", [
-//                     navbar3.act.first_ping(),
-//                     navbar3.act.ping(),
-//                     navbar3.act.pong()
-//                 ], [
-//                     "first_pong",
-//                     "pong",
-//                     "ping"
-//                 ])
-//             ]
-//         }, `
-//       <nav data-component="navbar">
-//         <ul>
-//           <li><a href="#">Home</a></li>
-//           <li><a href="#">Blog</a></li>
-//         </ul>
-//       </nav>
-//     `)
-//     ]
-// });
+            return [
+                assert("action function responds", timer.talk_to_alarm(), "all good"),
+            ]
+        }, `
+    `)
+    ]
+});
