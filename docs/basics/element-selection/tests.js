4d3ef34fc3856ad99e8f22816efe76d9dc12a74d
Adam Grant <hello@adamgrant.me>
2022-08-09 21:53:56 -0700

    More tests

--- docs/basics/element-selection/tests.js
+++ docs/basics/element-selection/tests.js
@@ -1,47 +1,29 @@
-// doc({
-//     attach_id: "basics/actions",
-//     tests: [
-//         test("Actions", () => {
-//             let navbar = new Component("navbar");
+import Component from "../../../mozart.js";
+let timer = new Component("timer");
 
-//             navbar.act.ping = (q) => {
-//                 return ["pong", q("li").length];
-//             }
+timer.get_hello = function() {
+    return this.q(".hello").innerHTML;
+}
 
-//             let navbar2 = new Component("navbar2");
-//             navbar2.act = {
-//                 ping: (q) => { return "pong" },
-//                 pong: (q) => { return "ping" }
-//             }
+timer.get_element_out_of_scope = function() {
+    return this.q(".goodbye");
+}
 
-//             let navbar3 = new Component("navbar3");
-//             navbar3.act.first_ping = (q) => "first_pong";
-//             navbar3.act = {
-//                 ping: (q) => { return "pong" },
-//                 pong: (q) => { return "ping" }
-//             }
-
-//             return [
-//                 assert("action function responds", navbar.act.ping()[0], "pong"),
-//                 assert("action function employs the scoped selector", navbar.act.ping()[1], 2),
-//                 assert("actions can be assigned with an entire object", [navbar2.act.ping(), navbar2.act.pong()], ["pong", "ping"]),
-//                 assert("single and entire object declaration work gracefully", [
-//                     navbar3.act.first_ping(),
-//                     navbar3.act.ping(),
-//                     navbar3.act.pong()
-//                 ], [
-//                     "first_pong",
-//                     "pong",
-//                     "ping"
-//                 ])
-//             ]
-//         }, `
-//       <nav data-component="navbar">
-//         <ul>
-//           <li><a href="#">Home</a></li>
-//           <li><a href="#">Blog</a></li>
-//         </ul>
-//       </nav>
-//     `)
-//     ]
-// });
+doc({
+    attach_id: "basics/element-selection",
+    tests: [
+        test("Selecting an element", () => {
+            return [
+                assert("Can select scoped element", timer.get_hello(), "hello world"),
+                assert("Does not select element out of scope", timer.get_element_out_of_scope(), undefined),
+            ]
+        }, `
+        <div data-component="timer">
+          <p class="hello">hello world</p>
+        </div>
+        <div data-component="not-the-timer">
+          <p class="goodbye">You shouldn't see this.</p>
+        </div>
+    `)
+    ]
+});
