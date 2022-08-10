4c0585a910fef59cee1ddc5947a59b9150850280
Adam Grant <hello@adamgrant.me>
2022-08-09 19:57:40 -0700

    One full test suite

--- docs/basics/create-a-component/tests.js
+++ docs/basics/create-a-component/tests.js
@@ -3,9 +3,29 @@ doc({
     attach_id: "basics/create-a-component",
     tests: [
         test("Mozart basics", () => {
-            let navbar = new Component("navbar");
+            let dropdown_button = new Component("dropdown_button");
 
-            return [ assert("navbar exists", typeof(navbar), "object") ]
-        })
+            return [ 
+                assert("dropdown_button component exists", typeof(dropdown_button), "object"),
+                assert("dropdown_button has query_selector", !!dropdown_button.q, true),
+                assert("dropdown_button has store", !!dropdown_button.store, true),
+                assert("dropdown_button element found", dropdown_button.me.dataset.test, "123")
+            ]
+        },`
+        <button data-component="dropdown_button" data-test="123">
+            <span class="title">Open Menu</span>
+            <ul>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/blog">Blog</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+            </ul>
+        </button>
+        `)
     ]
 });
