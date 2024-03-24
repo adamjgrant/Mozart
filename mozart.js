fbfb4239749c86ef3ad199f6cde61ff3c55dd5d7
Your Name <you@example.com>
2024-03-23 17:08:25 -0700

    Removing clickquery

--- mozart.js
+++ mozart.js
@@ -40,10 +40,6 @@ class Component {
                 return elements.length > 1 ? Array.from(elements) : elements[0];
             },
 
-            // TODO: Optional clickQuery?
-            // https://github.com/adamjgrant/clickQuery
-            // o=function(s){m=this;m.length=0;m.splice=Function;m.t=document.querySelectorAll(s);for(i=0;i<m.t.length;i++)m.push(m.t[i]);return m};r=o.prototype,p=Array.prototype,r.length=0,r.push=p.push,r.splice=p.splice;r.click=function(f){for(i=0,m=this;i<m.t.length;i++){z=m.t[i],z.addEventListener('click',f);}return m};$=function(s){return new o(s)}
-
             get me()       { return this.q(); },
             register(name) { this.name = name; },
         }
